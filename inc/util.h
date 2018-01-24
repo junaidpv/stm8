64607571ae350c0ba541732d2baec93ae3ba73d9
Junaid PV <junaid@detlus.com>
2018-01-24 14:09:03 +0530

    Make macros look like funtions and protect from indirect includes.

--- inc/util.h
+++ inc/util.h
@@ -1,2 +1,9 @@
-#define SET_BIT(register_8, bit) (register_8 |= (1 << bit))
-#define CLEAR_BIT(register_8, bit) (register_8 &= ~(1 << bit))
+#ifndef UTIL_H
+#define UTIL_H
+
+
+#define set_bit(register_8, bit) (register_8 |= (1 << bit))
+#define clear_bit(register_8, bit) (register_8 &= ~(1 << bit))
+#define toggle_bit(register_8, bit) (register_8 ^= (1 << bit))
+
+#endif /* UTIL_H */
