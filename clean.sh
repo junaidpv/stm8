d5bccde083320f19af5e7c129c91b7098cbcd59f
Junaid PV <junaid@detlus.com>
2018-01-24 14:09:50 +0530

    Script to remove object and other intermediate files from library source directory.

---
+++ clean.sh
@@ -0,0 +1,3 @@
+BASEDIR=$(dirname "$0")
+
+rm -f $BASEDIR/src/*.map $BASEDIR/src/*.asm $BASEDIR/src/*.rel $BASEDIR/src/*.ihx $BASEDIR/src/*.o $BASEDIR/src/*.sym $BASEDIR/src/*.lk $BASEDIR/src/*.lst $BASEDIR/src/*.rst $BASEDIR/src/*.cdb $BASEDIR/src/*.bin
